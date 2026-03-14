2ec1cf972b40c8cc42b0d1c2fd7982358d09fd50
Sahil <sahilkapali@gmail.com>
2026-03-14 20:47:05 +0545

    Add updated task and user features

--- backend/task_manager/tasks/views.py
+++ backend/task_manager/tasks/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+# GET tasks + CREATE task
+class TaskListCreateView(generics.ListCreateAPIView):
+
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+# GET single task + UPDATE + DELETE
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
